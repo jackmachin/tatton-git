7731565f77fb6fb707ec09f0fb6c9114c196c298
jackmachin <jack.machin125@gmail.com>
2015-05-13 15:47:38 +0100

    Home page

---
+++ page-home.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ Template Name: Home page
+ *
+ * This is the home page template.
+*/
+?>
+
+<?php get_header(); ?>
+
+    <div id="content" class="wrapper">
+
+        <div id="inner-content" class="container">
+
+            <div class="main"  role="main">
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                    <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+                        <div class="entry-content cf" itemprop="articleBody">
+                            <?php
+                                // the content (pretty self explanatory huh)
+                                the_content();
+                            ?>
+                        </div>
+
+                    </article>
+
+                <?php endwhile; else : ?>
+
+                <?php get_template_part ('partials/no-post-found');?>
+
+                <?php endif; ?>
+
+            </div>
+
+            <?php get_sidebar(); ?>
+
+        </div>
+
+    </div>
+
+<?php get_footer(); ?>
