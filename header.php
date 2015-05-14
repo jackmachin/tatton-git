500ac560b3d6b103d7c96ba196fce44f7178c5f2
jackmachin <jack.machin125@gmail.com>
2015-05-14 09:32:22 +0100

    LOGO

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
             <div id="inner-header">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <p id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/tatton-logo.png" alt="<?php bloginfo('name'); ?>" width="160" height="175"/></a>
 
                     <?php // if you'd like to use the site description you can un-comment it below ?>
                     <?php // bloginfo('description'); ?>
