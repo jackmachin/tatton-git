82b19b43090d5307f5337cb7f315a1fdabda1465
jackmachin <jack.machin125@gmail.com>
2015-05-14 09:04:41 +0100

    Sawdust

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,7 +1,7 @@
 <nav role="navigation">
 
   <?php wp_nav_menu(array(
-    'container' => 'div',                           // remove nav container
+    'container' => false,                           // remove nav container
     'container_class' => 'wrapper',                 // class of container (should you choose to use it)
     'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
     'menu_class' => 'nav top-nav cf',               // adding custom nav class
